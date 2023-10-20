852e218bee32648d8be6fe0beb499a8ccc9d27b9
unknown <abderahmanamarnis333@gmail.com>
2023-10-20 10:10:08 +0100

    game

--- add.js
+++ add.js
@@ -16,7 +16,7 @@ function allowDrop(ev) {
 let id =["d1","d2","d3","d4","d5","d6","d7","d8","d9","d10","d11","d12"]
 var k7al = document.getElementsByClassName("k7al")
 var byad = document.getElementsByClassName("byad")
-var dowara = document.getElementsByClassName("cirle") 
+var player1 = document.getElementsByClassName("cirle") 
 var player2 = document.getElementsByClassName("player2") 
 let bord = byad.length + k7al.length
 for(i=0 ; i<k7al.length;i++){
@@ -27,12 +27,17 @@ for(i=0 ; i<k7al.length;i++){
 // function idi(){
 //   D = Math.floor(Math.random()*8)
 // }
-for(i=0;i<dowara.length;i++){
-  dowara[i].setAttribute("id" , id[i])
-dowara[i].setAttribute("draggable" , "true")
-dowara[i].setAttribute("ondragstart" , "drag(event)")
+for(i=0;i<player1.length;i++){
+  player1[i].setAttribute("id" , id[i])
+player1[i].setAttribute("draggable" , "true")
+player1[i].setAttribute("ondragstart" , "drag(event)")
 player2[i].setAttribute("id" , "player2"+i)
 player2[i].setAttribute("draggable" , "true")
 player2[i].setAttribute("ondragstart" , "drag(event)")
 
 }
+
+var parentDiv = document.querySelectorAll('.row');
+var indexxx = Array.from(parentDiv).indexOf(parentDiv[7])
+console.log(indexxx);
+ 
